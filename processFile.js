40567c7e2fe5bdd14f8080a0ada636dd77b656e5
Mo <33751344+V1254@users.noreply.github.com>
2020-12-05 20:48:13 +0000

    finished

--- processFile.js
+++ processFile.js
@@ -2,22 +2,31 @@ const fs = require("fs");
 const readLine = require("readline");
 const Excel = require("exceljs");
 
-const maximumRows = 1048576; // excels row limit per sheet
+const maximumRows = 1000000; // excels row limit per sheet
 
-module.exports = (sheetName, sheet, fileName, workbook) =>
-  new Promise(async (resolve, reject) => {
+module.exports = (sheetName, sheet, fileName, workbook) => {
+  let currentSheet = sheet;
+  let lineCount = 0;
+  let columns = [];
+  let newSheetCount = 1;
+
+  return new Promise((resolve, reject) => {
     const fileInStream = fs.createReadStream(fileName);
     const rl = readLine.createInterface({
       input: fileInStream,
       crlfDelay: Infinity,
     });
+    rl.on("line", async (line) => {
+      const toPersist = line.split(/\t/).map((el) => {
+        if (!Number(el)) {
+          return el;
+        }
+        return Number(el);
+      });
 
-    let currentSheet = sheet;
-    let lineCount = 0;
-    let columns = [];
-    let newSheetCount = 1;
-    rl.on("line", (line) => {
-      if (lineCount === maximumRows) {
+      if (lineCount >= maximumRows) {
+        console.log(`reached maximum row count, reseting to 0`);
+        lineCount = 0;
         // commit the current sheet
         currentSheet.commit();
         // create a new sheet and set as the current
@@ -26,32 +35,28 @@ module.exports = (sheetName, sheet, fileName, workbook) =>
         // add the columns
         currentSheet.addRow(columns).commit();
 
-        // reset the counters
-        newSheetCount++;
-        lineCount++;
         // continue as per usual now with this line data
-      }
+        console.log(`reached maximum row count,created new sheet ${sheetName}-${newSheetCount}`);
+        ++lineCount;
+      } else {
+        // console.log(`currentLineCount: ${lineCount}`);
 
-      const toPersist = line.split(/\t/).map((el) => {
-        if (!Number(el)) {
-          return el;
+        if (!columns.length) {
+          console.log(
+            `Adding columns none found -data: lineCount:${lineCount}, columns:${columns.length}`
+          );
+          // this is the column
+          columns = toPersist.slice();
         }
-        return Number(el);
-      });
 
-      if (lineCount === 0) {
-        // this is the column
-        columns = toPersist.slice();
+        currentSheet.addRow(toPersist).commit();
+        ++lineCount;
       }
-
-      currentSheet.addRow(toPersist).commit();
-      lineCount++;
     });
 
     rl.on("close", async () => {
       console.log(`closed file read`);
-      fileInStream.destroy();
-      currentSheet.commit(); // close the worksheet
+      await currentSheet.commit(); // close the worksheet
       resolve();
     });
 
@@ -61,3 +66,4 @@ module.exports = (sheetName, sheet, fileName, workbook) =>
       reject();
     });
   });
+};
--- test.js
+++ test.js
@@ -2,52 +2,99 @@ const glob = require("glob");
 const addToSheet = require("./processFile");
 const Excel = require("exceljs");
 
-const APO_TYPES = {
-  // ADDITIVE: "_Additive.",
-  // DOMINANT: "_Dominant.",
-  GENOTYPIC: "_Genotypic.",
-  // RECESIVE: "_Recessive.",
-};
+const types = ["_Additive.", "_Dominant.", "_Genotypic.", "_Recessive."];
+// const types = ["_Genotypic."];
 const BASE_PATH = "D:/v3/";
 const OUTPUT_PATH = "D:/v3/parsed/";
 
 // navigate to the path
 process.chdir(BASE_PATH);
 
-const wrapper = async () => {
-  for await ([, val] of Object.entries(APO_TYPES)) {
-    console.log(`Processing ${val} sheets`);
-    const workbook = new Excel.stream.xlsx.WorkbookWriter({
-      filename: OUTPUT_PATH + val.split("_")[1] + ".xlsx",
-    });
-    // const outputFileName = val.split("_")[1]; // done
-    let index = 1;
-    await glob(`*${val}*.linear`, async (err, files) => {
+const globWrapper = (filesToMatch, currentFileName) =>
+  new Promise((resolve, resject) => {
+    glob(`*${filesToMatch}*.linear`, async (err, files) => {
+      let currentWorkBook = new Excel.stream.xlsx.WorkbookWriter({
+        filename: OUTPUT_PATH + currentFileName + ".xlsx",
+      });
+      let index = 1;
+      const _gentypicCount = 4; // only fit 3 genotypic files per book so it doesn't get huge
+      let _createGntypicCount = 0;
       for (const fileName of files) {
-        console.log(`Processing ${index} of ${files.length}`);
+        console.log(`\nProcessing ${index} of ${files.length}\n`);
         index++;
         const splitByDot = fileName.split("."); // Additive.transform.., .glim
         const sheetName = splitByDot[1]
           .replace(/transformed_EDU_/, "")
           .replace(/transformed_APO_/, "")
           .replace(/transformed_MIG_/, "");
-        console.log(`Processing sheet : ${sheetName} of ${fileName}`);
+        if (filesToMatch === "_Genotypic." && _createGntypicCount >= _gentypicCount) {
+          console.log(
+            `Hit max Files for current workbook [${_createGntypicCount}|${_gentypicCount}] saving current workbook: ${currentFileName}.xlsx`
+          );
+          await currentWorkBook.commit();
 
-        const sheet = workbook.addWorksheet(sheetName);
-        await addToSheet(sheetName, sheet, fileName, workbook);
+          console.log(`Finished saving ${currentFileName}.xlsx`);
 
-        // await workbook.commit();
+          // nextFileName
+          const splitCurrentFileName = currentFileName.split("-");
+          if (splitCurrentFileName.length === 1) {
+            // first addition we are doing
+            currentFileName = currentFileName + "-1";
+          } else {
+            currentFileName =
+              splitCurrentFileName[0] + "-" + (parseInt(splitCurrentFileName[1]) + 1);
+          }
+
+          // new workbook
+          currentWorkBook = new Excel.stream.xlsx.WorkbookWriter({
+            filename: OUTPUT_PATH + currentFileName + ".xlsx",
+          });
+
+          _createGntypicCount = 0;
+        }
+
+        console.log(`Adding file: ${fileName} to ${currentFileName}.xlsx`);
+
+        const sheet = currentWorkBook.addWorksheet(sheetName);
+        await addToSheet(sheetName, sheet, fileName, currentWorkBook);
+
+        console.log(`\n\n======================================\n`);
+
+        // increment the count after adding to the workbook for this file
+        if (filesToMatch === "_Genotypic.") {
+          _createGntypicCount++;
+        }
       }
-
-      //   const first2 = files.slice(0, 2);
-      //   console.log(`files: `, files);
-      //   await processFile(first2[1], OUTPUT_PATH + val + ".xlsx", "test");
-      //   // await processFile(first2[2], OUTPUT_PATH + val + ".xlsx");
-      //   console.log(`finished processing ??`);
-      console.log(`Finished Processing all ${val} sheets, committing`);
-      await workbook.commit();
+      console.log(`Finished Processing all ${filesToMatch} sheets, committing`);
+      await currentWorkBook.commit();
+      resolve();
     });
+  });
+
+const wrapper = async () => {
+  for (let i = 0; i < types.length; i++) {
+    const currentType = types[i];
+    let currentFileName = currentType.split("_")[1].replace(".", "");
+    console.log(`======================================`);
+    console.log(`\nStarting Work on ${currentType} files \n`);
+    // let currentWorkBook = new Excel.stream.xlsx.WorkbookWriter({
+    //   filename: OUTPUT_PATH + currentFileName + ".xlsx",
+    // });
+
+    await globWrapper(currentType, currentFileName);
   }
+
+  // for await ([, val] of Object.entries(APO_TYPES)) {
+  //   let currentFileName = val.split("_")[1].replace(".", "");
+  //   console.log(`Processing ${val} sheets`);
+  //   let currentWorkBook = new Excel.stream.xlsx.WorkbookWriter({
+  //     filename: OUTPUT_PATH + currentFileName + ".xlsx",
+  //   });
+  //   // const outputFileName = val.split("_")[1]; // done
+  //   let index = 1;
+  //   const _gentypicCount = 3; // only fit 3 genotypic files per book so it doesn't get huge
+  //   let _createGntypicCount = 0;
+  // }
 };
 
 wrapper();
